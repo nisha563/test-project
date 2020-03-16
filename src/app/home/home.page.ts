798da722ca9c1ba313d6f3feb3f8adf51cf5b7f1
nisha563 <boon.office@boonsoftware.co.in>
2020-03-16 15:08:48 +0530

    merge error:

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { ToastController } from '@ionic/angular';
+import {  AnimationController } from '@ionic/angular';
+import { trigger, style, animate, transition, group, query, animateChild } from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public displayAchievement: boolean = false;
+  public segment:string="home";
+  constructor(
+    private toastCtrl: ToastController
+  ) {}
 
-  constructor() {}
+  test(){
+
+    this.toastCtrl.create({
+      message: 'Achievement unlocked!',
+      duration: 2000
+    }).then((toast) => {
+      toast.present();
+    });
+
+    this.displayAchievement = true;
+
+    setTimeout(() => {
+      this.displayAchievement = false;
+    }, 3000);
+
+  }
 
 }
